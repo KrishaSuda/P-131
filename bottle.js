274d3cd4d93e036908a9144eeb9f9f2780c52dc7
KrishaSuda <85116052+KrishaSuda@users.noreply.github.com>
2022-06-03 14:39:31 +0530

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,33 @@
+img = "";
+status = "";
+
+function preload(){
+    img = loadImage('bottle.webp');
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420)
+    fill("#ffffff");
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);  
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    } else {
+        console.log(results);
+    }
+
+}
